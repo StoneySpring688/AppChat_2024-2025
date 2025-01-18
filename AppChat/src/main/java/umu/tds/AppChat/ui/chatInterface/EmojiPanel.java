0b93d8a5db2281b405eca51eea8512321fb38e89
StoneySpring688 <albertozapatamira50@gmail.com>
2025-01-18 15:35:57 +0100

    v2 Emojis

--- AppChat/src/main/java/umu/tds/AppChat/ui/chatInterface/EmojiPanel.java
+++ AppChat/src/main/java/umu/tds/AppChat/ui/chatInterface/EmojiPanel.java
@@ -4,21 +4,56 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
-
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.SwingConstants;
+
+import net.miginfocom.swing.MigLayout;
+import umu.tds.AppChat.ui.ScrollBar;
 
 public class EmojiPanel extends JPanel{
 
 	private static final long serialVersionUID = 1L;
 	
     private int round;
+    private RoundPanel emojiPanel;
+    private JScrollPane scroll;
 	
     public EmojiPanel() {
-        this.setLayout(null);
-        this.setOpaque(false);
-        //setBackground(new Color(255, 94, 98, 76));
-        setRound(30);
+    	this.setLayout(new MigLayout("fill, insets 0", "[grow]", "[grow]"));
+    	
+    	//panel emojis
+    	emojiPanel = new RoundPanel();
+        emojiPanel.setLayout(new  MigLayout("wrap 6, gapx 100, gapy 100","[][][][][][]","[][]"));
+        emojiPanel.setOpaque(false);
+        emojiPanel.setRound(30);
+        
+        //añadir emojis al panel
+        this.cargarEmojis();
+        
+        //añadir el panel dentro de un JScrollPane
+        scroll = new JScrollPane(emojiPanel);
+		scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		scroll.setVerticalScrollBar(new ScrollBar());
+		scroll.setBorder(BorderFactory.createEmptyBorder());
+		scroll.getViewport().setOpaque(false);
+        scroll.setOpaque(false);
+        scroll.getVerticalScrollBar().setOpaque(false);
+
+        //añadir el JScrollPane al EmojiPanel   
+        this.add(scroll, "grow");
 	}
+    
+    private void cargarEmojis() {
+    	for (int i = 1; i <= 50; i++) {
+            JLabel emojiLabel = new JLabel("\uD83D\uDE0A"); // Emoji de ejemplo (😊)
+            emojiLabel.setHorizontalAlignment(SwingConstants.CENTER);
+            emojiPanel.add(emojiLabel);
+        }
+    }
     
 	public int getRound() {
         return round;
@@ -31,7 +66,6 @@ public class EmojiPanel extends JPanel{
 	
 	@Override
     protected void paintComponent(Graphics grphcs) {
-		//super.paintComponent(grphcs);
         Graphics2D g2 = (Graphics2D) grphcs.create();
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         int width = getWidth();
