b4bf593529a8c05928cab75c358588e987f6c485
StoneySpring688 <albertozapatamira50@gmail.com>
2025-02-11 01:33:55 +0100

    se eliminó un warning en AppFrame

--- AppChat/src/main/java/umu/tds/AppChat/ui/AppFrame.java
+++ AppChat/src/main/java/umu/tds/AppChat/ui/AppFrame.java
@@ -5,7 +5,6 @@ import java.awt.Color;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
